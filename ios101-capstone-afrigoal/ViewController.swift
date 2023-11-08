cb6dd182f8a3b42715efa9077ddff44bc54d4038
Qetsia Nkulu <qetsiankulu@gmail.com>
2023-11-07 17:57:21 -0800

    Initial Commit

---
+++ ios101-capstone-afrigoal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios101-capstone-afrigoal
+//
+//  Created by Qetsia Nkulu  on 11/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
