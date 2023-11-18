1971d85c4b5e3ca92b2dc62d403f8b3b8299dcc4
Qetsia Nkulu <qetsiankulu@gmail.com>
2023-11-17 16:51:10 -0800

    created groups_screen

---
+++ GroupCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupCellTableViewCell.swift
+//  ios101-capstone-afrigoal
+//
+//  Created by Qetsia Nkulu  on 11/17/23.
+//
+
+import UIKit
+
+class GroupCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ios101-capstone-afrigoal/Group.swift
@@ -0,0 +1,13 @@
+//
+//  Group.swift
+//  ios101-capstone-afrigoal
+//
+//  Created by Qetsia Nkulu  on 11/17/23.
+//
+
+import Foundation
+
+struct Group {
+    let name: String
+    let teams: [String]
+}
